edc13c699b7df73307c8e4253b8023f6e7192125
Fbada006 <ferdinandkamata@yahoo.com>
2020-04-13 21:55:18 +0300

    Set up bottom navigation

--- app/src/main/java/com/disruption/cookcentral/MainActivity.java
+++ app/src/main/java/com/disruption/cookcentral/MainActivity.java
@@ -3,6 +3,11 @@ package com.disruption.cookcentral;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        setUpBottomNavMenu();
+    }
+
+    private void setUpBottomNavMenu() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
