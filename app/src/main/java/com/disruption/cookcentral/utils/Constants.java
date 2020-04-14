c5ec87e479f1989dd0209299c2c3daf6a54ef8e4
Fbada006 <ferdinandkamata@yahoo.com>
2020-04-14 21:23:22 +0300

    Set up worker

--- app/src/main/java/com/disruption/cookcentral/utils/Constants.java
+++ app/src/main/java/com/disruption/cookcentral/utils/Constants.java
@@ -3,4 +3,6 @@ package com.disruption.cookcentral.utils;
 public final class Constants {
     public static final String BASE_URL = "https://api.spoonacular.com/";
     public static final String INGREDIENT_BASE_URL = "https://spoonacular.com/cdn/ingredients_100x100/";
+    public static final String CHANNEL_ID = "com.disruption.cookcentral.foodjoke";
+    public static final int NOTIFICATION_ID = 1;
 }
