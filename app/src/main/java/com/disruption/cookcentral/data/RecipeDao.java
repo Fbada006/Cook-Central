c9d90bd11661545d55245750fa82dd6b70373229
Fbada006 <ferdinandkamata@yahoo.com>
2020-04-14 22:09:29 +0300

    Set up widget

--- app/src/main/java/com/disruption/cookcentral/data/RecipeDao.java
+++ app/src/main/java/com/disruption/cookcentral/data/RecipeDao.java
@@ -16,6 +16,9 @@ public interface RecipeDao {
     @Query("SELECT * FROM recipes")
     LiveData<List<CachedRecipe>> loadAllFavs();
 
+    @Query("SELECT * FROM recipes")
+    List<CachedRecipe> loadAllFavsForWidget();
+
     @Query("SELECT * FROM recipes WHERE id = :id")
     LiveData<CachedRecipe> loadRecipeById(int id);
 
---
+++ app/src/main/java/com/disruption/cookcentral/widget/FavsWidgetRemoteViewsFactory.java
@@ -0,0 +1,66 @@
+package com.disruption.cookcentral.widget;
+
+import android.content.Context;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.disruption.cookcentral.data.RecipeDatabase;
+import com.disruption.cookcentral.models.CachedRecipe;
+
+import java.util.List;
+
+public class FavsWidgetRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+
+    private Context mContext;
+    private List<CachedRecipe> mRecipeList;
+    private RecipeDatabase mRecipeDatabase;
+
+    public FavsWidgetRemoteViewsFactory(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate() {
+        mRecipeDatabase = RecipeDatabase.getInstance(mContext);
+    }
+
+    @Override
+    public void onDataSetChanged() {
+        mRecipeList = mRecipeDatabase.recipeDao().loadAllFavsForWidget();
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public int getCount() {
+        return mRecipeList.size();
+    }
+
+    @Override
+    public RemoteViews getViewAt(int i) {
+        return null;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 0;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+}
