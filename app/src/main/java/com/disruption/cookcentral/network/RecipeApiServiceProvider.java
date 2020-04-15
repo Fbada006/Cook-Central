7ac066a370c6e0eb13527abfc62f186baa846308
Fbada006 <ferdinandkamata@yahoo.com>
2020-04-15 14:38:34 +0300

    Remove logging

--- app/src/main/java/com/disruption/cookcentral/network/RecipeApiServiceProvider.java
+++ app/src/main/java/com/disruption/cookcentral/network/RecipeApiServiceProvider.java
@@ -1,7 +1,5 @@
 package com.disruption.cookcentral.network;
 
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -10,12 +8,6 @@ import static com.disruption.cookcentral.utils.Constants.BASE_URL;
 
 public class RecipeApiServiceProvider {
     private static Retrofit retrofit;
-    private static HttpLoggingInterceptor sHttpLoggingInterceptor =
-            new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC);
-
-    private static OkHttpClient sOkHttpClient = new OkHttpClient.Builder()
-            .addInterceptor(sHttpLoggingInterceptor)
-            .build();
 
     private static Retrofit retrofitInstance() {
         if (retrofit == null) {
@@ -23,7 +15,6 @@ public class RecipeApiServiceProvider {
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                    .client(sOkHttpClient)
                     .build();
         }
         return retrofit;
