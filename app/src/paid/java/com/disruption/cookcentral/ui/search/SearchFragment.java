7deaa1b6c99df6c34cc893e9a2211596b4ae2fad
Fbada006 <ferdinandkamata@yahoo.com>
2020-04-15 14:49:04 +0300

    Further flavour sorting for the search fragment

---
+++ app/src/paid/java/com/disruption/cookcentral/ui/search/SearchFragment.java
@@ -0,0 +1,108 @@
+package com.disruption.cookcentral.ui.search;
+
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.disruption.cookcentral.R;
+import com.disruption.cookcentral.databinding.FragmentSearchBinding;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SearchFragment extends Fragment implements SearchView.OnQueryTextListener {
+
+    private FragmentSearchBinding mBinding;
+    private SearchViewModel mSearchViewModel;
+
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false);
+        mBinding.searchView.setOnQueryTextListener(this);
+
+        SearchedRecipesAdapter adapter = new SearchedRecipesAdapter();
+        initRv(adapter);
+
+        mSearchViewModel = new ViewModelProvider(this).get(SearchViewModel.class);
+
+        mSearchViewModel.getSearchedRecipes().observe(this, recipeResponseResource -> {
+            switch (recipeResponseResource.status) {
+                case SUCCESS:
+                    if (recipeResponseResource.data != null && !recipeResponseResource.data.getResults().isEmpty()) {
+                        adapter.submitList(recipeResponseResource.data.getResults());
+                        mBinding.progressBar.setVisibility(View.GONE);
+                        mBinding.infoText.setVisibility(View.GONE);
+                    } else {
+                        mBinding.progressBar.setVisibility(View.GONE);
+                        mBinding.infoText.setVisibility(View.VISIBLE);
+                        mBinding.infoText.setText(getString(R.string.no_data_to_display));
+                    }
+                    break;
+                case ERROR:
+                    mBinding.progressBar.setVisibility(View.GONE);
+                    mBinding.infoText.setVisibility(View.VISIBLE);
+                    mBinding.infoText.setText(getString(R.string.error_has_occurred, recipeResponseResource.message));
+                    break;
+                case LOADING:
+                    mBinding.progressBar.setVisibility(View.VISIBLE);
+                    mBinding.infoText.setVisibility(View.GONE);
+                    break;
+            }
+        });
+
+        return mBinding.getRoot();
+    }
+
+    private void initRv(SearchedRecipesAdapter adapter) {
+        RecyclerView recyclerView = mBinding.recyclerView;
+        recyclerView.setLayoutManager(new GridLayoutManager(requireContext(), 1));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        if (!TextUtils.isEmpty(query)) {
+            mSearchViewModel.searchRecipe(query);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Objects.requireNonNull(((AppCompatActivity) Objects.requireNonNull(getActivity())).getSupportActionBar()).hide();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Objects.requireNonNull(((AppCompatActivity) Objects.requireNonNull(getActivity())).getSupportActionBar()).show();
+    }
+}
